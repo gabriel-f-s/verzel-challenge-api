256f990a89de76c6ffb69bde787e4dcb88fe44c7
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-08-12 08:34:35 -0300

    feat: scaffold core feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './database/prisma.module';
+import { IdentityModule } from './identity/identity.module';
+import { IntegrationsModule } from './integrations/integrations.module';
+import { EventsModule } from './events/events.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, IdentityModule, IntegrationsModule, EventsModule, TicketsModule],
   controllers: [AppController],
   providers: [AppService],
 })
