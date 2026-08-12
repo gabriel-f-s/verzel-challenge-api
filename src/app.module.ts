61ab6ef73c7fe9a9ea185478ef887c18283fd3ab
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-08-12 11:50:54 -0300

    feat: setup global JWT authentication guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,24 @@ import { IdentityModule } from './identity/identity.module';
 import { IntegrationsModule } from './integrations/integrations.module';
 import { EventsModule } from './events/events.module';
 import { TicketsModule } from './tickets/tickets.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 
 @Module({
-  imports: [PrismaModule, IdentityModule, IntegrationsModule, EventsModule, TicketsModule],
+  imports: [
+    PrismaModule,
+    IdentityModule,
+    IntegrationsModule,
+    EventsModule,
+    TicketsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
