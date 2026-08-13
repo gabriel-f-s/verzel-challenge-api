af177619d719ddb229388242495d2e44c5b4ef08
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-08-13 14:56:19 -0300

    feat: Configure EventsModule

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { EventController } from './controllers/event.controller';
+import { EventService } from './services/event.service';
+import { PrismaModule } from '../database/prisma.module';
+import { EventRepository } from './repositories/event.repository';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  controllers: [EventController],
+  providers: [EventService, EventRepository],
+  exports: [EventService],
+})
 export class EventsModule {}
