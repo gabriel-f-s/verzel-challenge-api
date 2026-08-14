e0a06334e67d224c6aba62327f02258012a81041
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-08-14 17:19:50 -0300

    feat: Configure TicketsModule

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TicketController } from './controllers/ticket.controller';
+import { TicketService } from './services/ticket.service';
+import { TicketRepository } from './repositories/ticket.repository';
+import { QrCodeService } from './services/qr-code.service';
+import { PrismaModule } from '../database/prisma.module';
+import { EventsModule } from '../events/events.module';
 
-@Module({})
+@Module({
+  imports: [PrismaModule, EventsModule],
+  controllers: [TicketController],
+  providers: [TicketService, TicketRepository, QrCodeService],
+  exports: [TicketService],
+})
 export class TicketsModule {}
